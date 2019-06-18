59a581b9ccd6aa8dc1f2e0809fe7ac8310d5bd47
yangjj <1542103461@qq.com>
2019-06-18 17:36:44 +0800

    update

--- packages/debounce/index.js
+++
@@ -1,8 +0,0 @@
-import Debounce from './src/main';
-
-/* istanbul ignore next */
-Debounce.install = function (Vue) {
-  Vue.component(Debounce.name, Debounce);
-};
-
-export default Debounce;
--- packages/throttle/index.js
+++
@@ -1,8 +0,0 @@
-import Throttle from './src/main';
-
-/* istanbul ignore next */
-Throttle.install = function (Vue) {
-  Vue.component(Throttle.name, Throttle);
-};
-
-export default Throttle;
