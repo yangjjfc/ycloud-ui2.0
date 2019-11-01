bf482530b66637f15c564394519e0000c4089268
yangjj <1542103461@qq.com>
2019-11-01 15:57:15 +0800

    deplay

--- src/index.js
+++ src/index.js
@@ -11,13 +11,18 @@ import Table from '../packages/table/index.js';
 import Sku from '../packages/sku/index.js';
 import StatusTips from '../packages/status-tips/index.js';
 import CollapseTransition from 'ycloud-ui/src/transitions/collapse-transition';
-
 import Http from 'ycloud-ui/src/utils/axios/index';
 import * as Tools from 'ycloud-ui/src/utils/global';
 import double from 'ycloud-ui/src/utils/double';
+import summaries from 'ycloud-ui/src/utils/summaries';
 import emitter from 'ycloud-ui/src/mixins/emitter';
 import formValid from 'ycloud-ui/src/mixins/formValid';
+import focus from 'ycloud-ui/src/mixins/focus';
+import linkTo from 'ycloud-ui/src/mixins/linkTo';
+import tableAutoHeight from 'ycloud-ui/src/mixins/tableAutoHeight';
 import boxer from 'ycloud-ui/src/directives/boxer';
+import autoHeight from 'ycloud-ui/src/directives/autoHeight';
+import money from 'ycloud-ui/src/fliters/money';
 import { Environment, changeEnvironment } from 'ycloud-ui/src/config';
 
 const components = [
@@ -40,6 +45,8 @@ const install = function (Vue, opts = {}) {
   });
   Vue.prototype.Http = Http;
   Vue.directive('boxer', boxer);
+  Vue.directive('autoHeight', autoHeight);
+  Vue.filter('money', money);
 }; 
 
 /* istanbul ignore if */
@@ -49,12 +56,16 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 export {
   emitter,
+  focus,
+  linkTo,
+  tableAutoHeight,
   formValid,
   Http,
   Environment,
   changeEnvironment,
   Tools,
-  double
+  double,
+  summaries
 };
 
 export default {
